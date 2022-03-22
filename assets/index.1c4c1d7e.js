6bd87aaa780a566cf36c7f8c31c9ebae94652381
AylaAsia-zhaopan <87957117+AylaAsia-zhaopan@users.noreply.github.com>
2022-03-22 08:47:07 +0000

    Deploying to gh-pages from @ AylaAsia-zhaopan/ayla-antd@f28e807e06ad192884cd3c3d197e57671e1accf2 🚀

--- assets/index.1c4c1d7e.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor.7a9c323d.js";import{D as e}from"./field-names.7bf4bd22.js";import"./index.30edbad6.js";var m={setup(){},render(){return r(e,null,null)}};export{m as default};
