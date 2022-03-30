72ea58ad4ba6a12f35ef41b6acb18264bc7c83e2
zybzzc <zybzzc@gmail.com>
2022-03-30 06:59:17 +0000

    Deploying to gh-pages from @ ayla-web/ayla-antd@e6cc260d9f3cfbd3ec035befa97704a04f56d899 🚀

--- assets/index.5c49f947.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor.44d980da.js";import{D as e}from"./field-names.3efc0e91.js";import"./index.d41bdb67.js";var m={setup(){},render(){return r(e,null,null)}};export{m as default};
