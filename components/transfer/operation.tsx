e1bcc3133ba5414b516004490cb04d5ab6239c50
zybzzc <zybzzc@gmail.com>
2022-04-25 15:41:25 +0800

    feat(transfer): 样式

--- components/transfer/operation.tsx
+++ components/transfer/operation.tsx
@@ -38,7 +38,7 @@ const Operation: FunctionalComponent<TransferOperationProps> = props => {
   return (
     <div class={className} style={style}>
       <Button
-        type="primary"
+        type={disabled || !rightActive ? undefined : 'primary'}
         size="small"
         disabled={disabled || !rightActive}
         onClick={moveToRight}
@@ -48,7 +48,7 @@ const Operation: FunctionalComponent<TransferOperationProps> = props => {
       </Button>
       {!oneWay && (
         <Button
-          type="primary"
+          type={disabled || !leftActive ? undefined : 'primary'}
           size="small"
           disabled={disabled || !leftActive}
           onClick={moveToLeft}
